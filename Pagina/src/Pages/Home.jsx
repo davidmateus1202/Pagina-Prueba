4d0e3b837e4bd94e99b3ec32575c859b982e567e
davidmateus1202 <mateusdavid417@gmail.com>
2024-08-17 18:36:31 -0500

    responsive

--- Pagina/src/Pages/Home.jsx
+++ Pagina/src/Pages/Home.jsx
@@ -21,33 +21,33 @@ export default function Home() {
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 transition={{ duration: 0.5 }}
-                className="flex flex-col h-auto w-full bg-slate-100 px-56 items-center justify-center">
+                className="flex flex-col h-auto w-full bg-slate-100 px-5 xl:px-56 items-center justify-center">
 
 
                 <section id="inicio" className="flex flex-row h-[500px] w-full bg-gradient-to-tr from-[#4373ee] to-[#1a2151] items-center justify-center  shadow-xl">
 
-                    <h1 className="text-white font-semibold text-3xl">
+                    <h1 className="text-white font-semibold text-sm  xl:text-3xl">
                         Arquitectura de la nube y modelos de servicio
                     </h1>
-                    <img src={meet} className="w-auto h-[70%]" />
+                    <img src={meet} className="w-auto h-[40%] xl:h-[70%] " />
                 </section>
 
-                <section id="ventajas" className="flex flex-row w-full h-[500px] items-center justify-center px-36">
-                    <div className="flex flex-col w-1/3 h-full items-center justify-center gap-y-5">
+                <section id="ventajas" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full h-[500px] items-center justify-center px-10 overflow-y-auto">
+                    <div className="flex flex-col w-auto h-full items-center justify-center gap-y-5">
                         <img src={virtual} width={'100px'} />
                         <p className="font-extralight px-5 text-justify">
                             Proporciona a los usuarios recursos virtualizados, como máquinas virtuales, almacenamiento y redes. El usuario tiene control sobre el sistema operativo y las aplicaciones, pero no sobre la infraestructura física subyacente.
                         </p>
                     </div>
 
-                    <div className="flex flex-col w-1/3 h-full items-center justify-center gap-y-5">
+                    <div className="flex flex-col w-auto h-full items-center justify-center gap-y-5">
                         <img src={server} width={'90px'} />
                         <p className="font-extralight px-5 text-justify">
                             Ofrece un entorno de desarrollo preconfigurado que permite a los desarrolladores crear, ejecutar y gestionar aplicaciones sin preocuparse por la gestión de la infraestructura. Los usuarios controlan las aplicaciones y los datos, pero no la infraestructura subyacente.
                         </p>
                     </div>
 
-                    <div className="flex flex-col w-1/3 h-full items-center justify-center gap-y-5">
+                    <div className="flex flex-col w-auto h-full items-center justify-center gap-y-5">
                         <img src={dataImage} width={'90px'} />
                         <p className="font-extralight px-5 text-justify">
                             Es un tipo de nube que se comparte entre varias organizaciones con intereses comunes. Permite a los usuarios beneficiarse de un entorno colaborativo en el que los recursos y servicios se comparten de manera segura entre los participantes.
